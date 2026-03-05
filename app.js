77620b2df0c9c5cbae3943ba40e9d300dccd12d7
Guillem <guillemarana21@gmail.com>
2026-03-05 21:14:17 +0100

    funcionara ahora

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+import authRouter from './routes/auth.js';
+import taskRouter from './routes/task.js';
+import groupRouter from './routes/group.js';
+import profileRouter from './routes/profile.js';
+import groupTasksRouter from './routes/groupTask.js';
+
+import validateToken from './middleware/authToken.js';
+import cors from 'cors';
+
+const app = express();
+const host = process.env.HOST;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: '*', // en desarrollo permite todo
+  }),
+);
+
+app.use('/auth/', authRouter);
+
+app.use(validateToken);
+
+app.use('/tasks', taskRouter);
+app.use('/profile', profileRouter);
+app.use('/groups', groupRouter);
+app.use('/groups', groupTasksRouter);
+
 // ! START SERVER FIRST (Recomendado para Railway)
 const PORT = process.env.PORT || 3000;
 
